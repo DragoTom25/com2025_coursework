840625e491da68cd49dea1e1bd49fd3a5de69c51
DragoTom25 <ra1845@hotmail.co.uk>
2019-12-15 14:45:32 +0000

    Added index to DParking

--- db/migrate/20191215143956_create_d_parkings.rb
+++ db/migrate/20191215143956_create_d_parkings.rb
@@ -1,10 +1,10 @@
 class CreateDParkings < ActiveRecord::Migration[5.2]
   def change
     create_table :d_parkings do |t|
-      t.string :car_plate
-      t.string :disability
-      t.string :name
-      t.string :gender
+      t.string :car_plate, null: false
+      t.string :disability, null: false
+      t.string :name, null: false
+      t.string :gender, null: false 
 
       t.timestamps
     end
